dbfc63ab32c8df72e0df082475cfff4f22536c7c
Tonydang223 <danghnh@dipro.vn>
2023-10-24 11:59:26 +0700

    first commit

---
+++ inde.js
@@ -0,0 +1,30 @@
+const arrG = [
+  {
+    id: 1,
+    usrs: [
+      { id: "ad", name: "daj" },
+      { id: "ax", name: "dagggj" },
+      { id: "adx", name: "dajsfsf" },
+    ],
+  },
+  {
+    id: 2,
+    usrs: [
+      { id: "ax", name: "dagggj" },
+      { id: "adx", name: "dajsfsf" },
+    ],
+  },
+  {
+    id: 3,
+    usrs: [
+        { id: "ad", name: "daj" },
+        { id: "ax", name: "dagggj" },
+    ],
+  },
+];
+
+arrG.filter((v) => {
+  const cloneusr = v.usrs.map((v) => v.id);
+
+  return cloneusr.includes("ad");
+});
